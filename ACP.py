1fe3d8f3cf1bac69fa882d742204f21561379a60
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-03-21 15:42:59 -0800

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,10 @@
+bday1 = "01/3"
+bday2 = "06/15"
+bday3 = "03/14"
+bday4 = "08/27"
+bday5 = "12/28"
+print("Birthday 1 is stored as", bday1)
+print("Birthday 1 is stored as", bday2)
+print("Birthday 1 is stored as", bday3)
+print("Birthday 1 is stored as", bday4)
+print("Birthday 1 is stored as", bday5)
