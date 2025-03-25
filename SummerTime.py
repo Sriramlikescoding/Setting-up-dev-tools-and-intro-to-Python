ece2478975817b73b1dec6e4894378376fea5cb5
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-03-25 15:46:10 -0800

    Add files via upload

---
+++ SummerTime.py
@@ -0,0 +1,9 @@
+x = int(input("Input 1 integer addend: "))
+y = int(input("Input 1 integer addend: "))
+z = int(input("Input The Sum"))
+
+if x + y == z:
+    print("Your Math Ss Good And Your Answer Is Correct")
+else:
+    print("Incorrect")
+
