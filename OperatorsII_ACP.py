9db1fe74f25332a8841c3cb027b7282c0160df4b
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-03-28 15:36:22 -0800

    Add files via upload

---
+++ OperatorsII_ACP.py
@@ -0,0 +1,6 @@
+inp = str(input("Please enter a letter of the alphabet: "))
+
+if inp == "A" or "B" or "C" or "D" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "R" or "S" or "T" or "U" or "V" or "W" or "X" or "Y" or "Z" or "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z":
+    print("the symbol you entered is a letter of the alphabet")
+else:
+    print("The character you entered is not a letter of the alphabet")
