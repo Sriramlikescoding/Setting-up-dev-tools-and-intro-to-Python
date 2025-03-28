93e6eae717bb0f2c9a27013fca11936c88786adc
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-03-28 15:23:47 -0800

    Add files via upload

---
+++ If, Elif.py
@@ -0,0 +1,25 @@
+a = 12
+b = 10
+c = 0
+
+if a and b and c:
+    print("All the numbers have boolean values as true")
+else:
+    print("At least one number has a boolean value as false")
+
+
+a = 10
+b = -10
+c = 0
+
+if a > 0 or b > 0:
+    print("A or B is greater than zero")
+else:
+    print("No number is greater than zero")
+
+
+if b >0 or c>0:
+    print("Either B or C is greater than 0")
+else:
+    print("Neither B nor C is greater than 0")
+    
---
+++ Not_Logic_Operator.py
@@ -0,0 +1,23 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+b = "Coding"
+c = "Python"
+
+if b!=c:
+    print(b, 'and', c, "are different")
+
+a = 4
+b = 5
+
+if(a == 1) != (b == 5):
+    print("hello")
+
+a = int(input("Enter a number: "))
+
+if a%2 != 0:
+    print("This number is not even")
