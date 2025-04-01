039f805d6583cc5643c583cfc875df89aee7b243
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-04-01 15:35:35 -0800

    Add files via upload

---
+++ Nested Ifs After Class Project.py
@@ -0,0 +1,9 @@
+age = input("Enter your age")
+
+if age >= 10:
+    if age <= 20:
+        print("You are between 10 and 20")
+    else:
+        print("You are not between 10 and 20")
+else:
+    print("You are not between 10 and 20")
