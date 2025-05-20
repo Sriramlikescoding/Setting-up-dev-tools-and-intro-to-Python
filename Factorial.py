ee11ef9db8fa88a0115f06a093bbaa176d853fbb
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-05-20 15:35:22 -0800

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,13 @@
+def factorial(x):
+
+
+
+    if x == 0 or x == 1:
+        return 1
+    else:
+        return x*factorial(x-1)
+  
+print("The factorial of 0", factorial(0))
+print("The factorial of 4", factorial(4))
+print("The factorial of 5", factorial(5))
+print("The factorial of 10", factorial(10))
