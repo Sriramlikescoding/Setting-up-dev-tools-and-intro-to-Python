81271436eab0d1f99fc07e0c9c68d26b4e9e20ea
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-05-23 15:21:35 -0800

    Add files via upload

---
+++ Continue.py
@@ -0,0 +1,9 @@
+x = 10
+
+while x > 0:
+    x = x-1
+    if x == 5:
+        continue
+    print("Current Value is:", x)
+
+    
---
+++ Pass.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number"))
+
+if num%20 == 0:
+    print("Twist")
+elif num%15 == 0:
+    pass
+elif num%5 == 0:
+    print("Fizz")
+elif num%20 == 0:
+    print("Buzz")
