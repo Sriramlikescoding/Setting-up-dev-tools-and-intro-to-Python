f18c44a40ec3cda3e24467c49f19c4bb8a317228
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-06-24 15:21:35 -0800

    Add files via upload

---
+++ List Experiment.py
@@ -0,0 +1,12 @@
+EmptyList = []
+print(EmptyList)
+
+numbers = [1, 2, 3, 4, 5, 6]
+print(numbers)
+
+triples = [1, 2, 3] * 3
+print(triples)
+
+backwards = [1, 2, 3, 4, 5]
+backwards = backwards[::-1]
+print(backwards)
