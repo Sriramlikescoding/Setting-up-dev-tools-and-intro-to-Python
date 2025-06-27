6153c8cde860caf75420ccf53d9ebb6a53e441af
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-06-27 15:26:56 -0800

    Add files via upload

---
+++ Getting Started.py
@@ -0,0 +1,9 @@
+tuplex = (1, 2, 3, 4, 5, 6, 7, 8)
+print(tuplex + (0,9))
+
+tuplex2 = (50, 1, 2, 3, 4, )
+print(tuplex.count(50))
+
+tuplex3 = (1, 2, 3, 4, 5, 6, 7)
+tuplex3 = tuplex3[3:5]
+print(tuplex3)
