13f33f65bc04bfec5332e0ef362c53f7c9af18cc
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-07-04 15:19:17 -0800

    Add files via upload

---
+++ Functions on Sets.py
@@ -0,0 +1,15 @@
+my_set = {1, 2, 3, 4, 5}
+print(my_set)
+
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+my_set = {1, 2, 3, 4, 5, 5}
+print(my_set)
+
+num_set = {0, 1, 2, 3, 4}
+print("Original Set:" )
+print(num_set)
+num_set.pop()
+print("After removing the first element:")
+print(num_set)
