16f6cbe89ff9e1e622f7062cba8eddc8af576465
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-07-08 14:38:46 -0800

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,9 @@
+numbers = (10, 10, 10, 10, 10, 10, 10, 10)
+length = len(numbers)
+add = 0
+on = 0
+while length > 0:
+    add = add + numbers[on]
+    on =+ 1
+numbers = numbers/length
+print(numbers)
