cd226e2f6df2c8dfdfb9bd31b37a856a5aba1cd7
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-07-29 15:42:56 -0800

    Add files via upload

---
+++ ACP_word_reverse.py
@@ -0,0 +1,12 @@
+class INPUT():
+    def __init__(self, x = 'sriram'):
+        self.x = x
+        
+    def reverse(self):
+        for i in self.x:
+            x2 = len[self.x:-1]
+            print(x2)
+
+
+obj = INPUT()
+obj.reverse()
