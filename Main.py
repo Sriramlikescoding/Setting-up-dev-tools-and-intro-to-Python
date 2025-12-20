652e841839517d391fd9d06073b03d67b63dc439
Sriramlikescoding <sriram.m.8946@gmail.com>
2025-12-19 16:37:48 -0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
-import matplotlib.pyplot as m
-import numpy as n
-#(0,6), (6,250)
-x_points = n.array([0,6])
-y_points = n.array([6,250])
-m.plot(x_points,y_points)
-m.show()
+import sqlite3
+import pandas as pd
+database = "database.sqlite"
+conn = sqlite3.connect(database)
+print('databse connected succesfully')
+
+tables = pd.read_sql("""select name from sqlite_master where type = 'table'""", conn)
+print("Tables have been printed.")
+print(tables)
+
+
+#Player_Match
+
+Player_Data=pd.read_sql("""select * from Player_Match""", conn)
+print("data in player_match table is")
+print(Player_Data)
+conn.close()
