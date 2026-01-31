9ca486a16a61162057bec52ad6a8b3084a94efea
Sriramlikescoding <sriram.m.8946@gmail.com>
2026-01-30 16:46:03 -0800

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,18 +1,30 @@
-import numpy as np
+import matplotlib.pyplot as p
 import pandas as pd
+import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
-missingValues = ["NA", "na", "N/A", "", np.nan]
-df = pd.read_csv("Data.csv", na_values=missingValues)
-print(df)
-print("Missing_Values Count by Column")
+import warnings
+
+df = pd.read_csv("Heart.csv")
+df.head()
+print(df.head())
+df.shape
+print(df.shape)
+df.columns
+print(df.columns)
+df.describe()
+print(df.describe())
+df.isnull().sum()
 print(df.isnull().sum())
-print("Is There Any Missing Values")
-print(df.isnull().any())
-#sns.heatmap(df.isnull(), yticklabels= False, annot=True, cmap="coolwarm")
-#plt.title("Missing Values Heatmap")
-#plt.show()
-df_dropall = df.dropna(how="all")
-data_Cleaned = df_dropall.interpolate()
-print(data_Cleaned)
-data_Cleaned.to_csv("Cleaned_Data.csv", index = False)
+print(df.info())
+df.hist(figsize=(12,12),layout=(5,3))
+df.plot(kind="box",subplots=True, layout=(5,3), figsize=(12,12))
+p.figure(figsize=(6,4))
+sns.barplot(data=df,x="sex",y="chol",hue="target",palette="spring")
+p.show()
+print("sex value counts")
+print(df["sex"].value_counts())
+print("Target Value Counts")
+print(df["target"].value_counts())
+p.figure(figsize=(20,10))
+sns.heatmap(df.corr(),annot=True, cmap="terrain")
+p.show()
