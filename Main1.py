1e537fdf8eb94c102fa5e7ada9422b163193d8cc
Sriramlikescoding <sriram.m.8946@gmail.com>
2026-03-10 15:41:21 -0800

    Add files via upload

--- Main1.py
+++ Main1.py
@@ -1,28 +1,16 @@
-import matplotlib.pyplot as p
-import pandas as pd
-import seaborn as sns
 import numpy as np
+import pandas as pd
+import statistics as st
 
-data = pd.read_csv("Movie.csv")
-
-print(data.info())
-print(data.head(6))
-print(data.isnull().any())
+data = pd.read_csv("Bestsellers with categories.csv")
+data.head(6)
 print(data.isnull().sum())
-p.hist(data["Runtime"])
-p.xlabel("Movie Rating")
-p.ylabel("Count of Movies")
-p.show()
-data["Runtime"].unique()
-bins = np.arange(80,230,10)
-p.hist(data["Runtime"], edgecolor = "black", bins=bins, color = "g")
-p.xlabel("Movie Count")
-p.ylabel("Count of Movies")
-p.show()
-data["IMDB_Rating"].unique()
-bins = np.arange(8,10,0.20)
-p.hist(data["IMDB_Rating"], edgecolor = "black", bins = bins, color = "r")
-p.ylabel("Movie Countt")
-p.xlabel("Imdb Rating")
-p.xticks(bins)
-p.show()
+
+Mean_price = np.mean(data["Price"])
+print(Mean_price)
+
+Median_price = np.median(data["Price"])
+print(Median_price)
+
+mode_price = st.mode(data["Price"])
+print(mode_price)
